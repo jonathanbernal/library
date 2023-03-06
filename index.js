cc503b2711aed5349bd7abb21cb40e0f5623196b
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2023-03-05 23:06:22 -0500

    Add logic behavior to book status counters

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const bookShelf = document.querySelector('.bookshelf');
 const addBookButton = document.querySelector('.add-book-btn');
-const showAllBooksButton = document.querySelector('.show-all-books-btn');
 
 let booksInLibraryCount = 0;
 let totalBooksRead = 0;
@@ -23,8 +22,96 @@ class Book {
     getIsBookRead = () => this.isRead === 'true' || this.isRead === true;
 }
 
-const incrementBooksInLibraryCount = () => booksInLibraryCount += 1;
-const decrementBooksInLibraryCount = () => booksInLibraryCount -= 1;
+const incrementBooksInLibraryCounter = () => {
+    const bookCount = document.querySelector('.book-count');
+
+    if (booksInLibraryCount >= 0) {
+        booksInLibraryCount += 1;
+    }
+
+    bookCount.textContent = `${booksInLibraryCount}`;
+}
+
+const decreaseBooksInLibraryCounter = () => {
+    const bookCount = document.querySelector('.book-count');
+
+    if (booksInLibraryCount > 0) {
+        booksInLibraryCount -= 1;
+    }
+
+    bookCount.textContent = `${booksInLibraryCount}`;
+}
+
+const incrementTotalBooksRead = () => {
+    if (totalBooksRead >= 0) {
+        totalBooksRead += 1;
+    }
+}
+
+const decreaseTotalBooksRead = () => {
+    if (totalBooksRead > 0) {
+        totalBooksRead -= 1;
+    }
+}
+
+const incrementTotalBooksUnread = () => {
+    if (totalBooksUnread >= 0) {
+        totalBooksUnread += 1;
+    }
+}
+
+const decreaseTotalBooksUnread = () => {
+    if (totalBooksUnread > 0) {
+        totalBooksUnread -= 1;
+    }
+}
+
+function updateReadAndUnreadCountersOnReadValueToggle(bookReadValue) {
+    const readCounterElement = document.querySelector('.book-read-count');
+    const unreadCounterElement = document.querySelector('.book-unread-count');
+    
+    if(bookReadValue) {
+        incrementTotalBooksRead();
+        decreaseTotalBooksUnread();
+    } else {
+        decreaseTotalBooksRead();
+        incrementTotalBooksUnread();
+    }
+
+    readCounterElement.textContent = `${totalBooksRead}`;
+    unreadCounterElement.textContent = `${totalBooksUnread}`;
+}
+
+function updateReadBookCounterOnBookCreation(bookReadValue) {
+    const readCounterElement = document.querySelector('.book-read-count');
+    const unreadCounterElement = document.querySelector('.book-unread-count');
+
+    if(bookReadValue) {
+        incrementTotalBooksRead();
+    } else {
+        incrementTotalBooksUnread();
+    }
+
+    incrementBooksInLibraryCounter();
+    readCounterElement.textContent = `${totalBooksRead}`;
+    unreadCounterElement.textContent = `${totalBooksUnread}`;
+}
+
+function updateReadBookCountersOnBookDeletion(bookReadValue) {
+    const readCounterElement = document.querySelector('.book-read-count');
+    const unreadCounterElement = document.querySelector('.book-unread-count');
+    
+    if (bookReadValue && totalBooksRead > 0) {
+        totalBooksRead -= 1;
+    } 
+    // if the book was unread and there is at least 1 unread book in the library
+    if (!bookReadValue && totalBooksUnread > 0) {
+        totalBooksUnread -= 1;
+    }
+
+    readCounterElement.textContent = `${totalBooksRead}`;
+    unreadCounterElement.textContent = `${totalBooksUnread}`;
+}
 
 function createAddBookDialogElement() {
     const addBookDialogElement = document.createElement('dialog');
@@ -154,8 +241,11 @@ function createBookElementFromBookObject (bookObject) {
     // Update button values upon clicking on the Read/Unread button
     bookReadButton.addEventListener('click', () => {
         bookObject.toggleRead();
-        bookReadButton.setAttribute('value', bookObject.getIsBookRead());
-        bookReadButton.textContent = bookObject.getIsBookRead() ? 'Read' : 'Not read';
+        const isBookReadValue = bookObject.getIsBookRead();
+        bookReadButton.setAttribute('value', isBookReadValue);
+        // update read counters
+        updateReadAndUnreadCountersOnReadValueToggle(isBookReadValue);
+        bookReadButton.textContent = isBookReadValue ? 'Read' : 'Not read';
     });
 
     const bookDeleteButton = createButtonElement('Delete', 'button', 'btn', 'delete');
@@ -164,7 +254,11 @@ function createBookElementFromBookObject (bookObject) {
         deleteBookFromBookshelfElement(bookElement);
         // delete book object from the book library array
         deleteBookFromLibrary(bookObject);
+        // update book counter stats
+        decreaseBooksInLibraryCounter();
+        updateReadBookCountersOnBookDeletion(bookObject.getIsBookRead());
     });
+    
     bookButtonContainer.append(bookReadButton, bookDeleteButton);
     bookElement.append(bookButtonContainer);
 
@@ -195,7 +289,7 @@ function displayAddBookDialog() {
     const yesLabel = createLabelElementWithForAttribute('Yes', 'read-yes');
     const radioInputYes = createRadioInputElement('read', true, 'read-yes');
     // make yes be selected by default
-    //radioInputYes.setAttribute('checked', 'checked');
+    radioInputYes.setAttribute('checked', 'checked');
     const noLabel = createLabelElementWithForAttribute('No', 'read-no');
     const radioInputNo = createRadioInputElement('read', false, 'read-no');
 
@@ -229,6 +323,8 @@ function displayAddBookDialog() {
         if(!searchForDuplicateBooks(bookObject)) { // If the book does not exist
             addBookToLibrary(bookObject);
             createAndAppendBookElementToBookShelf(bookObject);
+            // update book scores
+            updateReadBookCounterOnBookCreation(bookObject.getIsBookRead());
             // close right after adding the book
             addBookDialog.close();
             document.body.removeChild(addBookDialog);
